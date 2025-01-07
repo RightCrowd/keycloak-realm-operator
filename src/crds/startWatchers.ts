b51e3b6208cab1f943558bae7032f1690d8b486c
Evert De Spiegeleer <evert.de.spiegeleer@gmail.com>
2025-01-07 08:49:21 +0100

    fix: minor fixes

--- src/crds/startWatchers.ts
+++ src/crds/startWatchers.ts
@@ -1,11 +1,23 @@
 import { startWatching as startWatchingManagedRealms } from "./managed-realms/handlers.ts";
 import { startWatching as startWatchingClientCredentials } from "./client-credentials/handlers.ts";
 
-import { scheduleJobs as scheduleSecretCleanupJobs, scheduleJobNow as scheduleSecretCleanupJobNow } from "./client-credentials/secretsCleanupQueue.ts";
-import { scheduleJobs as scheduleClientSecretsReconciliation, scheduleJobNow as _scheduleClientSecretsReconciliationNow } from "./client-credentials/reconciliationQueue.ts";
+import {
+  scheduleJobNow as scheduleSecretCleanupJobNow,
+  scheduleJobs as scheduleSecretCleanupJobs,
+} from "./client-credentials/secretsCleanupQueue.ts";
+import {
+  scheduleJobNow as _scheduleClientSecretsReconciliationNow,
+  scheduleJobs as scheduleClientSecretsReconciliation,
+} from "./client-credentials/reconciliationQueue.ts";
 
-import { scheduleJobs as scheduleRealmsCleanupJobs, scheduleJobNow as scheduleRealmsCleanupJobNow } from ".//managed-realms/cleanupQueue.ts";
-import { scheduleJobs as scheduleRealmsReconciliation, scheduleJobNow as _scheduleRealmsReconciliationNow } from "./managed-realms/reconciliationQueue.ts";
+import {
+  scheduleJobNow as scheduleRealmsCleanupJobNow,
+  scheduleJobs as scheduleRealmsCleanupJobs,
+} from "./managed-realms/cleanupQueue.ts";
+import {
+  scheduleJobNow as _scheduleRealmsReconciliationNow,
+  scheduleJobs as scheduleRealmsReconciliation,
+} from "./managed-realms/reconciliationQueue.ts";
 
 export const startAllWatchers = async () => {
   await Promise.all([
@@ -20,12 +32,12 @@ export const startAllQueues = async () => {
     scheduleClientSecretsReconciliation(),
 
     scheduleRealmsCleanupJobs(),
-    scheduleRealmsReconciliation()
+    scheduleRealmsReconciliation(),
   ]);
 
   // Run the cleanups immediatly
   await Promise.all([
     scheduleSecretCleanupJobNow(),
     scheduleRealmsCleanupJobNow(),
-  ])
+  ]);
 };
